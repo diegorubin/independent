4c9a6f150fefcb97b44dc083c9d374456df1602a
Diego Rubin <rubin.diego@gmail.com>
2014-05-11 17:47:56 -0300

    Correção de problemas que apareceram em produção.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,9 @@ module Independent
       #{Rails.root}/lib/behaviours
     }
 
-    config.i18n.default_locale = 'pt-BR'
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = :'pt-BR'
 
     config.enconding = 'utf-8'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,33 +4,23 @@ lock '3.2.1'
 set :application, 'independent'
 set :repo_url, 'git@github.com:diegorubin/independent.git'
 
-set :linked_files, %w{config/mongoid.yml}
+set :linked_files, %w{
+  config/mongoid.yml config/initializers/devise.rb config/initializers/setup_mail.rb
+}
 
-set :linked_dirs, %w{uploads}
+set :linked_dirs, %w{public/uploads themes}
 
 namespace :deploy do
 
-  desc 'Copy config files'
-  task :configure do
-    on roles(:app) do
-      execute :cp, "#{deploy_to}/shared/config/mongoid.yml", release_path.join('config')
-      execute :cp, "#{deploy_to}/shared/config/{devise.rb,setup_mail.rb}", release_path.join('config/initializers')
+  #desc 'Restart application'
+  #task :restart do
+  #  on roles(:app), in: :sequence, wait: 5 do
+  #    execute :touch, release_path.join('tmp/restart.txt')
+  #  end
+  #end
 
-      # remove versioned themes
-      execute :rm, '-rf', release_path.join('themes')
-      execute :ln, '-s', "#{deploy_to}/shared/themes", release_path.join('themes')
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :deploy, 'deploy:compile_assets'
-  after :deploy, 'deploy:cleanup'
+  before :deploy, 'deploy:compile_assets'
+  after :deploy, 'deploy:restart'
 
 end
 
