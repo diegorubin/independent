2ec60dc9fcd0a03ce879dff4232413a6fb03698e
Diego Rubin <rubin.diego@gmail.com>
2015-04-09 08:05:36 -0300

    fix build.

--- app/services/create_commentator.rb
+++ app/services/create_commentator.rb
@@ -6,10 +6,9 @@ class CreateCommentator
   def create
     commentator = Commentator.find_or_initialize_by(email: @comment.email) 
     commentator.name = @comment.name 
-    commentator.save if commentator.new_record? && @comment.published
-
-    if commentator.active && !@comment.published 
+    if !commentator.new_record? && commentator.active && !@comment.published 
       @comment.update_attribute(:published, true) 
     end
+    commentator.save if commentator.new_record? && @comment.published
   end
 end
