49ac94e6b65d9699b07507ca9a0b653b9c99df12
Diego Rubin <rubin.diego@gmail.com>
2015-04-15 12:38:28 -0300

    fix #61.

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -1,25 +1,8 @@
 module Filterable
   extend ActiveSupport::Concern
-
   included do
     def self.filter_by(filters)
-      ar_proxy = self.name.constantize
-
-      _filters = ar_proxy.admin_filters.keys
-      relation = ar_proxy
-      filters.each do |filter, value|
-        next if !_filters.include?(filter) || value.blank?
-
-        relation = case admin_filters[filter][:type]
-                   when 'regex'
-                     relation.where(filter => /#{value}/)
-                   else
-                     relation.where(filter => value)
-                   end
-
-      end
-
-      relation
+      Mongoid::Criteria.new(self).filter_by(filters)
     end
   end
 end
---
+++ config/initializers/mongoid_extensions.rb
@@ -0,0 +1,23 @@
+module MongoidExtenions
+  module CriteriaExtensions
+    def filter_by(filters)
+      criteria = self.clone
+      _filters = klass.admin_filters.keys
+      filters.each do |filter, value|
+        next if !_filters.include?(filter) || value.blank?
+
+        criteria = case admin_filters[filter][:type]
+                   when 'regex'
+                     criteria.where(filter => /#{value}/)
+                   else
+                     criteria.where(filter => value)
+                   end
+
+      end
+      criteria
+    end
+  end
+end
+
+Mongoid::Criteria.include(MongoidExtenions::CriteriaExtensions)
+
