cd29a5a45123b26aa92ae6fe7a260dcff61b8039
Diego Rubin <rubin.diego@gmail.com>
2015-05-01 16:09:13 -0300

    Datetime for filters.

--- config/initializers/mongoid_extensions.rb
+++ config/initializers/mongoid_extensions.rb
@@ -4,11 +4,14 @@ module MongoidExtenions
       criteria = self.clone
       _filters = klass.admin_filters.keys
       filters.each do |filter, value|
-        next if !_filters.include?(filter) || value.blank?
+        canon = canon_filter(filter)
+        next if !_filters.include?(canon) || value.blank?
 
-        criteria = case admin_filters[filter][:type]
+        criteria = case admin_filters[canon][:type]
                    when 'regex'
-                     criteria.where(filter => /#{value}/i)
+                     criteria_for_regex(criteria, filter, value)
+                   when 'datetime'
+                     criteria_for_datetime(criteria, filter, value)
                    else
                      criteria.where(filter => value)
                    end
@@ -16,6 +19,27 @@ module MongoidExtenions
       end
       criteria
     end
+
+    protected
+    def criteria_for_regex(criteria, filter, value)
+      begin
+        criteria.where(filter => /#{value}/i)
+      rescue RegexpError => e
+        criteria.where()
+      end
+    end
+
+    def criteria_for_datetime(criteria, filter, value)
+      canon = canon_filter(filter)
+      _filter = (filter =~ /ends_in/ ? canon.to_sym.lte : canon.to_sym.gte)
+      _value = DateTime.strptime(value, '%d/%m/%Y %H:%M')
+      criteria.where(_filter => _value)
+    end
+
+    def canon_filter(filter)
+      filter.gsub(/_(starts_in|ends_in)/, '')
+    end
+
   end
 end
 
