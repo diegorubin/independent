882d050408ce2c53fdc531fb11107dbf503f8a45
Diego Rubin <rubin.diego@gmail.com>
2017-01-30 07:19:18 -0200

    adding fonts to assets precompile.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,7 @@ Rails.application.config.assets.precompile += %w(
 
 Theme.all.each do |t| 
   Rails.application.config.assets.precompile += %W(#{t.label}.js #{t.label}.css)
+  Rails.application.config.assets.precompile += 
+    t.files.collect{ |f| f =~ /\/(.*?(scss|js|eot|woff|woff2))$/ && $1 }.compact
 end
 
